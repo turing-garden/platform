bc05070ac3205bb37d0542ee87236ac1e497d1ae
Igor Korovchenko <igsekor@gmail.com>
2022-09-15 13:36:15 +0300

    Add basic config for 11ty

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+module.exports = function(config) {
+  config.addFilter('ruDate', (value) => {
+    return value.toLocaleString('ru', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }).replace(' г.', '');
+  });
+
+  config.addFilter('shortDate', (value) => {
+    return value.toLocaleString('ru', {
+      month: 'short',
+      day: 'numeric'
+    }).replace('.', '');
+  });
+
+  config.addFilter('isoDate', (value) => {
+    return value.toISOString();
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'data',
+    },
+    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: false,
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    templateFormats: [
+      'md', 'njk'
+    ],
+  };
+};
